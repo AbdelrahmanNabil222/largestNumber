23efc1fa30afbe4e7c0713c73f5b3b28838b76eb
Abdelrahman nabil@example.com <AbdelrahmanNabil222@example.com>
2025-06-30 00:16:00 +0300

    Initial commit

---
+++ lib/largestNumber.dart
@@ -0,0 +1,15 @@
+// void main() {
+//   List num = [23, 45, 100, 35, -1, -20];
+//   //suppose the largest number id 23
+//   int largest = num[1];
+//   for (int i = 0; i < num.length; i++) {
+//     //while the loop is operating when come number of the list is > from the largest number i suppose :make the largest number = this came number
+//     if (num[i] > largest) {
+//       largest = num[i];
+//       print(largest);
+//     }
+//   }
+// }
+
+/////////////done
+///check if the input is largest or not
